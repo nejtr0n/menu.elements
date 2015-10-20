6fa27833fc5106cc9f55d75bfa919d36dd2bcd4a
Nejtr0n <a6y@xakep.ru>
2015-10-20 10:45:52 +0300

    Create

---
+++ component.php
@@ -0,0 +1,138 @@
+<?
+if(!defined("B_PROLOG_INCLUDED") || B_PROLOG_INCLUDED!==true) die();
+/** @var CBitrixComponent $this */
+/** @var array $arParams */
+/** @var array $arResult */
+/** @var string $componentPath */
+/** @var string $componentName */
+/** @var string $componentTemplate */
+/** @global CDatabase $DB */
+/** @global CUser $USER */
+/** @global CMain $APPLICATION */
+
+
+if(!isset($arParams["CACHE_TIME"]))
+	$arParams["CACHE_TIME"] = 36000000;
+
+$arParams["ID"] = intval($arParams["ID"]);
+$arParams["IBLOCK_ID"] = intval($arParams["IBLOCK_ID"]);
+$arParams["LIMIT"] = (intval($arParams["LIMIT"]) > 0) ? intval($arParams["LIMIT"]) : 10;
+
+$arResult["ITEMS"] = array();
+$arResult["ELEMENT_LINKS"] = array();
+
+// Cache menu
+if($this->StartResultCache())
+{
+	if(!CModule::IncludeModule("iblock")) {
+		$this->AbortResultCache();
+	} else {
+		$arFilter = array(
+			"IBLOCK_ID"=>$arParams["IBLOCK_ID"],
+			"GLOBAL_ACTIVE"=>"Y",
+			"IBLOCK_ACTIVE"=>"Y",
+			"IBLOCK_SECTION_ID" => (!empty($arParams["ID"])) ? $arParams["ID"] : false,
+		);
+		$arOrder = array(
+			"sort"=>"asc",
+		);
+		$arSelect = array (
+			"ID",
+			"NAME"
+		);
+		$rsElements = CIBlockElement::GetList($arOrder, $arFilter, false, Array("nPageSize"=>50), $arSelect);
+
+		if($arParams["IS_SEF"] !== "Y")
+			$rsElements->SetUrlTemplates("", $arParams["SECTION_URL"]);
+		else
+			$rsElements->SetUrlTemplates("", $arParams["SEF_BASE_URL"].$arParams["SECTION_PAGE_URL"]);
+		while($arElement = $rsElements->GetNext())
+		{
+			var_dump($arElement);
+
+			$arResult["SECTIONS"][] = array(
+				"ID" => $arElement["ID"],
+				"DEPTH_LEVEL" => $arElement["DEPTH_LEVEL"],
+				"~NAME" => $arElement["~NAME"],
+				"~SECTION_PAGE_URL" => $arElement["~SECTION_PAGE_URL"],
+			);
+			$arResult["ELEMENT_LINKS"][$arElement["ID"]] = array();
+		}
+		$this->EndResultCache();
+	}
+}
+
+//In "SEF" mode we'll try to parse URL and get ELEMENT_ID from it
+if($arParams["IS_SEF"] === "Y")
+{
+	$engine = new CComponentEngine($this);
+	if (CModule::IncludeModule('iblock'))
+	{
+		$engine->addGreedyPart("#SECTION_CODE_PATH#");
+		$engine->setResolveCallback(array("CIBlockFindTools", "resolveComponentEngine"));
+	}
+	$componentPage = $engine->guessComponentPath(
+		$arParams["SEF_BASE_URL"],
+		array(
+			"section" => $arParams["SECTION_PAGE_URL"],
+			"detail" => $arParams["DETAIL_PAGE_URL"],
+		),
+		$arVariables
+	);
+	if($componentPage === "detail")
+	{
+		CComponentEngine::InitComponentVariables(
+			$componentPage,
+			array("SECTION_ID", "ELEMENT_ID"),
+			array(
+				"section" => array("SECTION_ID" => "SECTION_ID"),
+				"detail" => array("SECTION_ID" => "SECTION_ID", "ELEMENT_ID" => "ELEMENT_ID"),
+			),
+			$arVariables
+		);
+		$arParams["ID"] = intval($arVariables["ELEMENT_ID"]);
+	}
+}
+
+if(($arParams["ID"] > 0) && (intval($arVariables["SECTION_ID"]) <= 0) && CModule::IncludeModule("iblock"))
+{
+	$arSelect = array("ID", "IBLOCK_ID", "DETAIL_PAGE_URL", "IBLOCK_SECTION_ID");
+	$arFilter = array(
+		"ID" => $arParams["ID"],
+		"ACTIVE" => "Y",
+		"IBLOCK_ID" => $arParams["IBLOCK_ID"],
+	);
+	$rsElements = CIBlockElement::GetList(array(), $arFilter, false, false, $arSelect);
+	if(($arParams["IS_SEF"] === "Y") && (strlen($arParams["DETAIL_PAGE_URL"]) > 0))
+		$rsElements->SetUrlTemplates($arParams["SEF_BASE_URL"].$arParams["DETAIL_PAGE_URL"]);
+	while($arElement = $rsElements->GetNext())
+	{
+		$arResult["ELEMENT_LINKS"][$arElement["IBLOCK_SECTION_ID"]][] = $arElement["~DETAIL_PAGE_URL"];
+	}
+}
+
+// Return results
+$aMenuLinksNew = array();
+$menuIndex = 0;
+$previousDepthLevel = 1;
+foreach($arResult["SECTIONS"] as $arSection)
+{
+	if ($menuIndex > 0)
+		$aMenuLinksNew[$menuIndex - 1][3]["IS_PARENT"] = $arSection["DEPTH_LEVEL"] > $previousDepthLevel;
+	$previousDepthLevel = $arSection["DEPTH_LEVEL"];
+
+	$arResult["ELEMENT_LINKS"][$arSection["ID"]][] = urldecode($arSection["~SECTION_PAGE_URL"]);
+	$aMenuLinksNew[$menuIndex++] = array(
+		htmlspecialcharsbx($arSection["~NAME"]),
+		$arSection["~SECTION_PAGE_URL"],
+		$arResult["ELEMENT_LINKS"][$arSection["ID"]],
+		array(
+			"FROM_IBLOCK" => true,
+			"IS_PARENT" => false,
+			"DEPTH_LEVEL" => $arSection["DEPTH_LEVEL"],
+		),
+	);
+}
+
+return $aMenuLinksNew;
+?>
---
+++ lang/ru/.description.php
@@ -0,0 +1,5 @@
+<?
+$MESS ['T_IBLOCK_DESC_MENU_ITEMS'] = "Пункты меню";
+$MESS ['T_IBLOCK_DESC_MENU_ITEMS_DESC'] = "Дополнение меню названиями элементов (из корня инфоблока или раздела)";
+$MESS ['MAIN_NAVIGATION_SERVICE'] = "Навигация";
+?>
